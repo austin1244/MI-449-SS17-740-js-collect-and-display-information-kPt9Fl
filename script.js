f9d14f32d2e6f8d5484cecbe8473872d41a4cfd7
Austin1244 <rixausti@msu.edu>
2017-02-05 15:48:10 -0500

    updating

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var profile = {
     message : function(){
       return "<h1>Hi, my name is "+this.firstname+" "+this.lastname+"</h1><p>"+this.descript+"</p>"+
              "<p>"+
-                  "If you're interested in a date, you can email me at "+ 
+                  "If you're interested in a date, you can email me at "+
                "<a href=mailto:"+this.email+" target=\"_blank\">"+this.email+"</a>"+
                  " or give me a call at "+
                "<a href=tel:"+this.phone+" target=\"_blank\">"+this.phone+"</a>."+
@@ -19,16 +19,16 @@ var profile = {
       profile.display();
     },
     display: function(){
-       document.getElementById("htmlDisplay").innerHTML = profile.message(); 
-       document.getElementById("codeDisplay").innerHTML = profile.message();
+       document.getElementById("htmlDisplay").innerHTML = profile.message();
+       document.getElementById("codeDisplay").innerHTML = "<xmp>"+profile.message() +"</xmp>";
     }
 }
 
 var input = document.getElementsByTagName("input");
 for(var i=0; i<input.length; i++){
-  input[i].addEventListener("keyup" ,profile.set , false);      
+  input[i].addEventListener("keyup" ,profile.set , false);
 }
 var textarea = document.getElementsByTagName("textarea");
 for(var i=0; i<textarea.length; i++){
-  textarea[i].addEventListener("keyup" ,profile.set , false);      
+  textarea[i].addEventListener("keyup" ,profile.set , false);
 }
